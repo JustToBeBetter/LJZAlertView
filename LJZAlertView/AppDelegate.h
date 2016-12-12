237fa5018e81239540e84e02f4f7dbf95bd96b30
JustToBeBetter <likeme@li.local>
2016-12-12 09:39:55 +0800

    Initial Commit

---
+++ LJZAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJZAlertView
+//
+//  Created by DBOX on 2016/12/12.
+//  Copyright © 2016年 DBOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJZAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJZAlertView
+//
+//  Created by DBOX on 2016/12/12.
+//  Copyright © 2016年 DBOX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
